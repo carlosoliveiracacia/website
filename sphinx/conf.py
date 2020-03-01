db0d408cf5eabe3b2eeea51b023835c8f1792177
Carlos Oliveira <carlos.oliveira.cacia@gmail.com>
2020-02-29 22:41:10 -0700

    Adding logo

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -48,6 +48,8 @@ html_theme_path = guzzle_sphinx_theme.html_theme_path()
 html_theme = 'sphinx_rtd_theme'
 html_sidebars = {'**':['localtoc.html','searchbox.html']}
 
+html_logo = "_static/logo.jpg"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
